61829c9079160b36e7823201644a2a1a702fc531
elshaday <yirga.elshaday@gmail.com>
2025-01-06 08:48:43 +0300

    Fix: recreate params on every request because of time queries

--- services/api.py
+++ services/api.py
@@ -2,17 +2,16 @@ import requests
 from datetime import datetime, timedelta
 from config import USGS_API_URL, ETHIOPIA_BOUNDS
 
-params = {
-    "format": "geojson",
-    "starttime": (datetime.utcnow() - timedelta(days=1)).isoformat(),
-    "endtime": datetime.utcnow().isoformat(),
-    **ETHIOPIA_BOUNDS,
-    "orderby": "time-asc",
-}
-
 
 def fetch_earthquake_data():
     try:
+        params = {
+            "format": "geojson",
+            "starttime": (datetime.utcnow() - timedelta(days=1)).isoformat(),
+            "endtime": datetime.utcnow().isoformat(),
+            **ETHIOPIA_BOUNDS,
+            "orderby": "time-asc"
+        }
         full_url = requests.Request("GET", USGS_API_URL, params=params).prepare().url
         response = requests.get(full_url)
         response.raise_for_status()
